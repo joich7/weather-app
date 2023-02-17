16cff1f8e30392766164889b871d48130ba8dbf7
Josiah Charley <josiah.charley@student.awesomeinc.org>
2023-02-17 15:00:08 +0000

    made adjustments to style

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ function init() {
     // first div with inputs(button and zip text box)
   make(/*Id:*/"inputsContainer",/*Parent Id:*/"main",/*Type:*/"div",/*class:*/"container p-5 text-center",/*Txt:*/"");
     make(/*Id:*/"weatherTitle",/*Parent Id:*/"inputsContainer",/*Type:*/"div",/*class:*/"container p-3 m-4 text-center display-1",/*Txt:*/"Weather App");
-    make(/*Id:*/"button",/*Parent Id:*/"inputsContainer",/*Type:*/"button",/*class:*/"",/*Inner Txt:*/"Get Weather");
+    make(/*Id:*/"button",/*Parent Id:*/"inputsContainer",/*Type:*/"button",/*class:*/"btn btn-outline-primary",/*Inner Txt:*/"Get Weather");
         selector("button").addEventListener('click', getData); 
     make(/*Id:*/"zipBox",/*Parent Id:*/"inputsContainer",/*Type:*/"input",/*class:*/"button",/*Inner Txt:*/"");
 
@@ -79,12 +79,12 @@ function weather() {
 
     make(/*Id:*/"cityCont",/*Parent Id:*/"dataContainer",/*Type:*/"div",/*class:*/"container m-3",/*Inner Txt:*/"")
       make(/*Id:*/"cityRow1", /*Parent Id:*/"cityCont",/*Type:*/ "div",/*class:*/"row text-center", "")
-        make(/*Id:*/"cityName", /*Parent Id:*/"cityRow1", /*Type:*/"div",/*class:*/"col-12 bg-success","City:")
-        make(/*Id:*/"cityNameContent",/*Parent Id:*/"cityRow1",/*Type:*/"div",/*class:*/"col-12 bg-danger",/*Inner Txt:*/state.city.name)
+        make(/*Id:*/"cityName", /*Parent Id:*/"cityRow1", /*Type:*/"div",/*class:*/"col-12  p-4 bg-success","City:")
+        make(/*Id:*/"cityNameContent",/*Parent Id:*/"cityRow1",/*Type:*/"div",/*class:*/"col-12 p-4 bg-danger",/*Inner Txt:*/state.city.name)
 
     make(/*Id:*/"tempCont",/*Parent Id:*/"dataContainer",/*Type:*/"div",/*class:*/"container text-center m-3",/*Inner Txt:*/"");
       make(/*Id:*/"tempHeader",/*Parent Id:*/"tempCont",/*Type:*/"div",/*class:*/"row text-center bg-success",/*Inner Txt:*/"");
-        make(/*Id:*/"tempText",/*Parent Id:*/"tempHeader",/*Type:*/"div",/*class:*/"col-12",/*Inner Txt:*/"Temperature");
+        make(/*Id:*/"tempText",/*Parent Id:*/"tempHeader",/*Type:*/"div",/*class:*/"col-12 p-4",/*Inner Txt:*/"Temperature");
      
       make(/*Id:*/"tempRow",/*Parent Id:*/"tempCont",/*Type:*/"div",/*class:*/"row p-5 bg-danger",/*Inner Txt:*/"");
         make(/*Id:*/"tempKValue",/*Parent Id:*/"tempRow",/*Type:*/"div",/*class:*/"col-4",/*Inner Txt:*/Math.round(tempK)+" K");
@@ -97,11 +97,11 @@ function weather() {
       make(/*Id:*/"conditionContent",/*Parent Id:*/"conditionContainer",/*Type:*/"div",/*class:*/"row p-5 bg-danger",/*Inner Txt:*/"");
         make(/*Id:*/"conditionText",/*Parent Id:*/"conditionContent",/*Type:*/"div",/*class:*/"col-12",/*Inner Txt:*/state.city.weather[0].description);
     
-    make(/*Id:*/"imgContainer",/*Parent Id:*/"dataContainer",/*Type:*/"div",/*class:*/"container",/*Inner Txt:*/"");
+    make(/*Id:*/"imgContainer",/*Parent Id:*/"dataContainer",/*Type:*/"div",/*class:*/"container h-10",/*Inner Txt:*/"");
       make(/*Id:*/"imgContTitle",/*Parent Id:*/"imgContainer",/*Type:*/"div",/*class:*/"row",/*Inner Txt:*/"");
         make(/*Id:*/"imgTitle",/*Parent Id:*/"imgContTitle",/*Type:*/"div",/*class:*/"col-4",/*Inner Txt:*/"Other Info.");
-      make(/*Id:*/"imgSrcContainer",/*Parent Id:*/"imgContainer",/*Type:*/"div",/*class:*/"row",/*Inner Txt:*/"");
-        make(/*Id:*/"imageSrc",/*Parent Id:*/"imgSrcContainer",/*Type:*/"img",/*class:*/"h-3",/*Inner Txt:*/"");
+      make(/*Id:*/"imgSrcContainer",/*Parent Id:*/"imgContainer",/*Type:*/"div",/*class:*/"row bg-dark",/*Inner Txt:*/"");
+        make(/*Id:*/"imageSrc",/*Parent Id:*/"imgSrcContainer",/*Type:*/"img",/*class:*/"",/*Inner Txt:*/"");
           selector("imageSrc").setAttribute("src", `http://openweathermap.org/img/wn/${state.city.weather[0].icon}@2x.png` );
 }
 
