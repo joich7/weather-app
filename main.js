51a000bf1111b06916fb5383467499cbded617b4
Josiah Charley <josiah.charley@student.awesomeinc.org>
2023-02-17 14:29:01 +0000

    added temp values and img but cant get it to work

--- main.js
+++ main.js
@@ -3,17 +3,10 @@ const API_KEY = 'd4515e3fab03f81b376b0e441c2efe9c';
 
 let state = {
       city: null,
-     
-      
+
 };
 
-
-
-
 function getData() {
-  
-  
-
   let zip = selector("zipBox").value;
   let zipcode = zip +",us";
   let options = {
@@ -22,8 +15,7 @@ function getData() {
          zip: zipcode,                                            
          appid: API_KEY,
      }                                    
-    }
- 
+  }
  
   axios.get('/weather', options)
     .then(function (response) {
@@ -40,6 +32,7 @@ function getData() {
       console.log(error);
       console.log("it failed");
       alert("area code invalid")
+
     })
     .then(function () {
       // always executed
@@ -48,62 +41,77 @@ function getData() {
 };
 
 
- const main = document.querySelector(".main");
+const main = document.querySelector(".main");
 
 function selector(ElementId){
 return document.getElementById(ElementId)
  }
 
- function make(IdName, Location, elementType, className, content)  {
-    
-     
+function make(IdName, Location, elementType, className, content)  {     
      const location = document.getElementById(Location);
      const newElement = document.createElement(elementType);
      newElement.setAttribute('id',IdName)
      newElement.setAttribute('class',className)
+
      if (content != null || content != ""){
         newElement.innerHTML = content;
      }
-     // HOW DO YOU PUT PARAMETER INTO CLAS SLIST AND NOT ADD''
-     location.appendChild(newElement)
- }
- 
- function init() {
 
+     location.appendChild(newElement)
+}
+ 
+function init() {
 
     // first div with inputs(button and zip text box)
-    make(/*Id:*/"inputsContainer",/*Parent Id:*/"main",/*Type:*/"div",/*class:*/"",/*Txt:*/"");
-      make(/*Id:*/"button",/*Parent Id:*/"inputsContainer",/*Type:*/"button",/*class:*/"",/*Inner Txt:*/"Get Weather");
+  make(/*Id:*/"inputsContainer",/*Parent Id:*/"main",/*Type:*/"div",/*class:*/"container p-5 text-center",/*Txt:*/"");
+    make(/*Id:*/"weatherTitle",/*Parent Id:*/"inputsContainer",/*Type:*/"div",/*class:*/"container p-3 m-4 text-center display-1",/*Txt:*/"Weather App");
+    make(/*Id:*/"button",/*Parent Id:*/"inputsContainer",/*Type:*/"button",/*class:*/"",/*Inner Txt:*/"Get Weather");
         selector("button").addEventListener('click', getData); 
+    make(/*Id:*/"zipBox",/*Parent Id:*/"inputsContainer",/*Type:*/"input",/*class:*/"button",/*Inner Txt:*/"");
 
-      make(/*Id:*/"zipBox",/*Parent Id:*/"inputsContainer",/*Type:*/"input",/*class:*/"button",/*Inner Txt:*/"");
-      make(/*Id:*/"dataContainer",/*Parent Id:*/"main",/*Type:*/"div",/*class:*/"container bg-danger",/*Inner Txt:*/"");
+  make(/*Id:*/"dataContainer",/*Parent Id:*/"main",/*Type:*/"div",/*class:*/"container text-white",/*Inner Txt:*/"");
  }
 
+
+
+//make function 
 function weather() {
-  //const tempK = state.city.main.temp;
-  //const tempF =(tempK - 273.15) * 9/5 + 32 ;
   const tempK = state.city.main.temp;
   const tempC = tempK - 273.15;
   const tempF = tempC*(9/5)+32;
+ 
   
-    make(/*Id:*/"cityCont",/*Parent Id:*/"dataContainer",/*Type:*/"div",/*class:*/"container",/*Inner Txt:*/"");
-      make("cityName", "cityCont", "div", "row", "City")
-      make("cityName", "cityCont", "div", "row", state.city.name)
+  
+    make(/*Id:*/"cityCont",/*Parent Id:*/"dataContainer",/*Type:*/"div",/*class:*/"container m-3",/*Inner Txt:*/"")
+      make(/*Id:*/"cityRow1", /*Parent Id:*/"cityCont",/*Type:*/ "div",/*class:*/"row text-center", "")
+        make(/*Id:*/"cityName", /*Parent Id:*/"cityRow1", /*Type:*/"div",/*class:*/"col-12 bg-success","City:")
+        make(/*Id:*/"cityNameContent",/*Parent Id:*/"cityRow1",/*Type:*/"div",/*class:*/"col-12 bg-danger",/*Inner Txt:*/state.city.name)
 
-    make(/*Id:*/"tempCont",/*Parent Id:*/"dataContainer",/*Type:*/"div",/*class:*/"container",/*Inner Txt:*/"");
-      make(/*Id:*/"tempHeader",/*Parent Id:*/"tempCont",/*Type:*/"div",/*class:*/"row",/*Inner Txt:*/"Temperature");
-      make(/*Id:*/"tempRow",/*Parent Id:*/"tempCont",/*Type:*/"div",/*class:*/"row",/*Inner Txt:*/"");
-        make(/*Id:*/"tempKValue",/*Parent Id:*/"tempRow",/*Type:*/"div",/*class:*/"col-4",/*Inner Txt:*/Math.floor(tempK)+" K");
-        make(/*Id:*/"tempFValue",/*Parent Id:*/"tempRow",/*Type:*/"div",/*class:*/"col-4",/*Inner Txt:*/Math.floor(tempF)+" F");
-        make(/*Id:*/"tempCValue",/*Parent Id:*/"tempRow",/*Type:*/"div",/*class:*/"col-4",/*Inner Txt:*/Math.floor(tempC)+" C");
+    make(/*Id:*/"tempCont",/*Parent Id:*/"dataContainer",/*Type:*/"div",/*class:*/"container text-center m-3",/*Inner Txt:*/"");
+      make(/*Id:*/"tempHeader",/*Parent Id:*/"tempCont",/*Type:*/"div",/*class:*/"row text-center bg-success",/*Inner Txt:*/"");
+        make(/*Id:*/"tempText",/*Parent Id:*/"tempHeader",/*Type:*/"div",/*class:*/"col-12",/*Inner Txt:*/"Temperature");
+     
+      make(/*Id:*/"tempRow",/*Parent Id:*/"tempCont",/*Type:*/"div",/*class:*/"row p-5 bg-danger",/*Inner Txt:*/"");
+        make(/*Id:*/"tempKValue",/*Parent Id:*/"tempRow",/*Type:*/"div",/*class:*/"col-4",/*Inner Txt:*/Math.round(tempK)+" K");
+        make(/*Id:*/"tempFValue",/*Parent Id:*/"tempRow",/*Type:*/"div",/*class:*/"col-4",/*Inner Txt:*/Math.round(tempF)+" F");
+        make(/*Id:*/"tempCValue",/*Parent Id:*/"tempRow",/*Type:*/"div",/*class:*/"col-4",/*Inner Txt:*/Math.round(tempC)+" C");
       
+    make(/*Id:*/"conditionContainer",/*Parent Id:*/"dataContainer",/*Type:*/"div",/*class:*/"container  text-center m-3",/*Inner Txt:*/"");
+      make(/*Id:*/"conditionTitle",/*Parent Id:*/"conditionContainer",/*Type:*/"div",/*class:*/"row p-5 bg-success",/*Inner Txt:*/"");
+        make(/*Id:*/"conditionTitleText",/*Parent Id:*/"conditionTitle",/*Type:*/"div",/*class:*/"col-12 ",/*Inner Txt:*/"Condition Outide:");
+      make(/*Id:*/"conditionContent",/*Parent Id:*/"conditionContainer",/*Type:*/"div",/*class:*/"row p-5 bg-danger",/*Inner Txt:*/"");
+        make(/*Id:*/"conditionText",/*Parent Id:*/"conditionContent",/*Type:*/"div",/*class:*/"col-12",/*Inner Txt:*/state.city.weather[0].description);
     
-      make(/*Id:*/"conditionCont",/*Parent Id:*/"dataContainer",/*Type:*/"div",/*class:*/"col-4",/*Inner Txt:*/"");
+    make(/*Id:*/"imgContainer",/*Parent Id:*/"dataContainer",/*Type:*/"div",/*class:*/"container",/*Inner Txt:*/"");
+      make(/*Id:*/"imgContTitle",/*Parent Id:*/"imgContainer",/*Type:*/"div",/*class:*/"row",/*Inner Txt:*/"");
+        make(/*Id:*/"imgTitle",/*Parent Id:*/"imgContTitle",/*Type:*/"div",/*class:*/"col-4",/*Inner Txt:*/"Other Info.");
+      make(/*Id:*/"imgSrcContainer",/*Parent Id:*/"imgContainer",/*Type:*/"div",/*class:*/"row",/*Inner Txt:*/"");
+        make(/*Id:*/"imageSrc",/*Parent Id:*/"imgSrcContainer",/*Type:*/"img",/*class:*/"",/*Inner Txt:*/"");
+          selector("imageSrc").setAttribute("src", state.city.weather[0].icon);
 }
  
- init()
 
+init()
  
 
 
